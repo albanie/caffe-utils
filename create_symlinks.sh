842edf32f709aec050008885fcbabf7cc6125c38
albanie <albanie@robots.ox.ac.uk>
2017-10-22 16:51:19 +0100

    update links

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,11 +1,43 @@
+# Obtain the path of this script
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+REFRESH=${1:-false}
+
 # set path to caffe dir
-CAFFE_DIR=${1:-"~/coding/libs/caffes/official-caffe"}
+CAFFE_DIR=${2:-"${HOME}/coding/libs/caffes/official-caffe"}
 echo "symlinking debug layer source code into ${CAFFE_DIR}"
+echo  "-----------"
 
-# add symlinks
-ln -s ./src/caffe/debug_solver.cpp \
-    "${CAFFE_DIR}/src/caffe/debug_solver.cpp"
-ln -s ./include/caffe/debug_solver.hpp \
-    "${CAFFE_DIR}/include/caffe/debug_solver.hpp"
+declare -a rel_paths=(
+"src/caffe/debug_solver.cpp"
+"include/caffe/debug_solver.hpp"
+)
 
-# then recompile
+# refresh previous symlinks
+for rel_path in "${rel_paths[@]}"
+do
+   src="${SCRIPTPATH}/${rel_path}"
+   dest="${CAFFE_DIR}/${rel_path}"
+   if [ $REFRESH = false ] ; then
+     if [ -f $src ] ; then
+       link=false
+     else
+       link=true
+     fi
+   else # refresh
+     echo "refreshing $dest"
+     link=true
+     if [ -L $dest ] ; then
+       echo "deleting $dest"
+       rm $dest
+     fi
+   fi
+   if [ $link = true ] ; then
+     echo "creating symlink ${src} -> ${dest}"
+     ln -s ${src} ${dest}
+   else
+     echo "${dest} exists, skipping... "
+   fi 
+done
